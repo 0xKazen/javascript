d692ef2b80764d451034f9d6b1787bf804e4ebf3
0xKazen <manjinderkang156@gmail.com>
2025-08-16 15:10:47 +0530

    Create comparison.js

---
+++ comparison.js
@@ -0,0 +1,17 @@
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let string1 = "hello"
+let string2 = " Kazen"
+
+let string3 = string1 + string2 ;
+
+console.log(string3);
