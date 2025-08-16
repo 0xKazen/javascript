52161a6d8f48dd0ce7b5490b43e05e101829810a
0xKazen <manjinderkang156@gmail.com>
2025-08-16 15:09:10 +0530

    Create 02_dataTypes.js

---
+++ 02_dataTypes.js
@@ -0,0 +1,24 @@
+// alert(3+3); we are using node.js, not browser
+
+console.log(3+3)
+
+console.log ("Kazen")
+
+
+let name = "Kazen"
+let age = 23
+let isLoggedIn = false 
+
+
+// Number =>  2 to power 53
+// bigInt => used for big numbers (stock Market )
+// String => ""
+// boolean => true/false
+// null => standalone Value (empty value)
+// undefined => NO value Assigned
+// symbol => unique
+
+// object
+
+console.log(typeof null); // null is a object
+console.log(typeof undefined);
