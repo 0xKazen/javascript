fd7db9c8efe074eb08fd44a466477e770fac3787
0xKazen <manjinderkang156@gmail.com>
2025-08-16 15:10:02 +0530

    Create 03_conversionOperation.js

---
+++ 03_conversionOperation.js
@@ -0,0 +1,26 @@
+let score = "kazen"
+
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+// "33abd" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = -11
+
+let booleanIsLoggedIn= Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Kazen" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
