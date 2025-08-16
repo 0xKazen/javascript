0c107d8009132f9eac55d23ee108abe06231089c
0xKazen <manjinderkang156@gmail.com>
2025-08-16 15:08:22 +0530

    Create 01_Variables.js

---
+++ 01_Variables.js
@@ -0,0 +1,20 @@
+const accountId = 132314
+let accountEmail = "kazen45@gmial.com"
+var accountPassword = "21314"
+accountCity = "minku"
+let accountState;
+
+
+// accountId = 3 // not allowed
+
+accountEmail = "trimp@gmail.com"
+accountPassword = 4534
+accountCity = "Mars";
+//
+console.log (accountId);
+
+/*
+ Prefer not to use Var 
+ because of issue in block scope and functional scope
+ */
+console.table([accountId,accountPassword,accountEmail,accountCity,accountState])
